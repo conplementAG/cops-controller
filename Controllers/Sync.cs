e5cbdfe7a275d3e05b60c3f27bafb8efcb47d701
Denis Biondic <denis.biondic@conplement.de>
2019-03-26 16:33:20 +0100

    feat: asp.net core application with namespace sync implementation

---
+++ Controllers/Sync.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ConplementAG.CopsController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SyncController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "I am alive.";
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Newtonsoft.Json.Linq.JObject value)
+        {
+            var namespaceName = value["parent"]["metadata"]["name"];
+            
+            JObject response = JObject.FromObject(
+                new {
+                    children = new[] {
+                        new {
+                            apiVersion = "v1",
+                            kind = "Namespace",
+                            metadata = new {
+                                name = namespaceName
+                            }
+                        }
+                    },
+                    status = new {
+                        namespaces = 1,
+                        name = namespaceName
+                    }
+                }
+            );
+
+            return Ok(response);
+        }
+    }    
+}
