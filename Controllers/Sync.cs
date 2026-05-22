9d53202caec915b9da1b13cc5ba85e1b37c34434
Christian Gottinger <Christian.Gottinger@conplement.de>
2026-05-22 14:09:02 +0200

    make cns admin role configurable with default cluster-admin

--- Controllers/Sync.cs
+++ Controllers/Sync.cs
@@ -1,4 +1,5 @@
 ﻿using ConplementAG.CopsController.Services;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
 using Serilog;
 using System;
@@ -11,6 +12,13 @@ namespace ConplementAG.CopsController.Controllers
     [ApiController]
     public class SyncController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public SyncController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         public ActionResult<string> Get()
         {
@@ -25,7 +33,8 @@ namespace ConplementAG.CopsController.Controllers
                 Log.Verbose("REQUEST===============================" + value.ToString(Newtonsoft.Json.Formatting.Indented));
 
                 var copsResource = CopsResourceFactory.Create(value);
-                var k8sResources = K8sResourceFactory.Create(copsResource);
+                var namespaceAdminRole = _configuration.GetValue("CopsController:NamespaceAdminRole", "cluster-admin");
+                var k8sResources = K8sResourceFactory.Create(copsResource, namespaceAdminRole);
 
                 JObject response = JObject.FromObject(
                     new
--- Services/K8sResourceFactory.cs
+++ Services/K8sResourceFactory.cs
@@ -7,22 +7,23 @@ namespace ConplementAG.CopsController.Services
 {
     public class K8sResourceFactory
     {
-        public static IList<object> Create(CopsResource resource)
+        public static IList<object> Create(CopsResource resource, string namespaceAdminRole = "cluster-admin")
         {
             var source = Convert.ChangeType(resource, resource.GetType());
 
             var method = typeof(K8sResourceFactory).GetMethod("Create", BindingFlags.NonPublic | BindingFlags.Static);
-            return (IList<object>)method.Invoke(null, new[] { source });
+            return (IList<object>)method.Invoke(null, new[] { source, namespaceAdminRole });
         }
 
         // Method used by reflection call
-        private static IList<object> Create(CopsNamespace copsNamespace)
+        private static IList<object> Create(CopsNamespace copsNamespace, string namespaceAdminRole)
         {
             return new List<object>
             {
                 new K8sNamespace(copsNamespace.Metadata.Name, copsNamespace.Spec.Project?.Name, copsNamespace.Spec.Project?.CostCenter),
                 K8sRoleBinding.NamespaceFullAccess(copsNamespace.Metadata.Name, copsNamespace.Spec.NamespaceAdminUsers,
-                    copsNamespace.Spec.NamespaceAdminServiceAccounts ??  new List<CopsAdminServiceAccountSpec>().ToArray()),
+                    copsNamespace.Spec.NamespaceAdminServiceAccounts ??  new List<CopsAdminServiceAccountSpec>().ToArray(),
+                    namespaceAdminRole),
                 K8sClusterRoleBinding.CopsNamespaceEditBinding(copsNamespace.Metadata.Name, copsNamespace.Spec.NamespaceAdminUsers,
                     copsNamespace.Spec.NamespaceAdminServiceAccounts ??  new List<CopsAdminServiceAccountSpec>().ToArray()),
                 K8sClusterRole.CopsNamespaceEdit(copsNamespace.Metadata.Name),
